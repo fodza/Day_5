6170dae182329e06a29f247411046742413e8764
Agnese <fodza@inbox.lv>
2018-05-10 19:59:19 +0300

    Day_5.2
    2.uzdevums

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -14,7 +14,23 @@ namespace Day_5
             Console.WriteLine("Ludzu ievadiet ka tevi sauc ?");
             dati = Console.ReadLine();
             Console.WriteLine("Tevi sauc " + dati);
+            
+            int gads1;
+            Console.WriteLine("Ludzu ievadiet gadu,kura sobrid dzivojam ");
+            string b = Console.ReadLine();
+            gads1 = Convert.ToInt16(b);
+            
+            int gads2;
+            Console.WriteLine("Ludzu ievadiet savu dzimsanas gadu");
+            string a = Console.ReadLine();
+            gads2 = Convert.ToInt16(a);
+
+            int c = gads1 - gads2;
+
+            Console.WriteLine("Jus esat " + c + " gadus vecs");
+
             Console.ReadLine();
+
 
         }
     }
